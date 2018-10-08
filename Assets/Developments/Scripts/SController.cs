ba8635a4da59586e4f281736e244282a27ddea6e
MiraiOka <uaofmsbct.9g@gmail.com>
2018-10-08 19:31:55 +0900

    set audio but can not connect

---
+++ Assets/Developments/Scripts/SController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SController : MonoBehaviour {
+
+    new AudioSource audio;
+
+	void Start () {
+        audio = GetComponent<AudioSource>();
+        audio.clip = Microphone.Start(null, true, 1, 44100);
+        audio.Play();
+	}
+
+	void Update () {
+        float[] data = new float[256];
+        float vol = 0;
+        audio.GetOutputData(data, 0);
+
+        foreach (float s in data)
+        {
+            vol += Mathf.Abs(s);
+        }
+
+        VolumeManager.instance.setSVolume(vol);
+	}
+}
---
+++ Assets/Developments/Scripts/VolumeManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeManager : MonoBehaviour {
+
+    public static VolumeManager instance;
+    [SerializeField] float sVolume;
+
+    [SerializeField] GameObject[] sFire;
+
+	void Start () {
+		if(instance == null)
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this);
+	}
+
+    public void setSVolume(float s)
+    {
+        sVolume = s;
+        for(int i = 0; i < 3; i++)
+        {
+            sFire[i].gameObject.transform.localScale = new Vector3(sVolume, sVolume, sVolume);
+        }
+
+    }
+}
