2fff42eb5fdafc71f04df88b9fa080f8627596e7
MiraiOka <uaofmsbct.9g@gmail.com>
2018-10-08 17:33:02 +0900

    set and move cube

---
+++ Assets/Developments/Scripts/CubeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        gameObject.transform.position += new Vector3(0.01f, 0, 0);
+	}
+}
